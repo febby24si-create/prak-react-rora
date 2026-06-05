b0c5929d2629b73433af27ddebb4803779653260
stifend <stifend24si@mahasiswa.pcr.ac.id>
2026-06-05 14:38:59 +0700

    commit keduaq

--- src/pages/FiturXyz.jsx
+++ src/pages/FiturXyz.jsx
@@ -1,11 +1,14 @@
 import PageHeader from "../components/PageHeader";
 
 export default function FiturXyz({ title, breadcrumb, children }) {
-  
-
   return (
     <div className="flex items-start justify-between px-2 pb-4">
-      <p>ini hakaman xyz</p>
+
+      <h1 className="text-2xl font-bold text-gray-800">{title}
+        Fitur XYZ adalah fitur terbaru yang kami luncurkan untuk meningkatkan
+        pengalaman pengguna. Dengan Fitur XYZ, Anda dapat melakukan berbagai hal
+        menarik seperti:
+      </h1>
     </div>
   );
 }
